313c6151481323546d691a5969d332a06d51f184
Jeremy Watt <jermwatt@gmail.com>
2026-03-20 11:08:43 -0700

    feat: wire PostHog provider and cookie consent banner into app shell

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,6 +2,8 @@
 
 import { AuthProvider } from '@/contexts/AuthContext'
 import { ErrorProvider } from '@/contexts/ErrorContext'
+import { PostHogWrapper } from '@/components/PostHogWrapper'
+import { PostHogIdentify } from '@/components/PostHogIdentify'
 import { ServiceWorkerRegistration } from '@/components/ServiceWorkerRegistration'
 import { OfflineIndicator } from '@/components/ui/OfflineIndicator'
 import { InstallPrompt } from '@/components/ui/InstallPrompt'
@@ -14,10 +16,13 @@ export function Providers({ children }: ProvidersProps) {
   return (
     <ErrorProvider>
       <AuthProvider>
-        <ServiceWorkerRegistration />
-        <OfflineIndicator />
-        <InstallPrompt />
-        {children}
+        <PostHogWrapper>
+          <PostHogIdentify />
+          <ServiceWorkerRegistration />
+          <OfflineIndicator />
+          <InstallPrompt />
+          {children}
+        </PostHogWrapper>
       </AuthProvider>
     </ErrorProvider>
   )
